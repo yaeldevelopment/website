3c2cee98c2169abeba3274fe410a6eb6075b2722
yaelajami2 <y0556722091@gmail.com>
2025-01-23 20:40:38 +0200

    order

--- Program.cs
+++ Program.cs
@@ -5,12 +5,24 @@ using Microsoft.Extensions.Hosting;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+
+
+// טעינת משתני סביבה
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddEnvironmentVariables(); // מוסיף משתני סביבה
+});
+
 // קוד הגדרת חיבור לבסיס נתונים
 var connectionString = Environment.GetEnvironmentVariable("UMBRACO_CONNECTION_STRING");
 if (!string.IsNullOrEmpty(connectionString))
 {
     builder.Configuration["ConnectionStrings:UmbracoDbDSN"] = connectionString;
+
 }
+
+// הדפס את מחרוזת החיבור לוודא שהיא קיימת
+
 
 // קביעת הגדרות Umbraco
 builder.CreateUmbracoBuilder()
@@ -22,9 +34,19 @@ builder.CreateUmbracoBuilder()
 
 WebApplication app = builder.Build();
 
-// הפעלת Umbraco
-await app.BootUmbracoAsync();
+// הגדרת אמצעי ניטור ובדיקת טעינת Umbraco
+try
+{
+    await app.BootUmbracoAsync();
 
+}
+catch (Exception ex)
+{
+
+    throw; // זרוק את השגיאה למעלה כדי שתופיע בלוגים
+}
+
+// שימוש באמצעי Umbraco
 app.UseUmbraco()
     .WithMiddleware(u =>
     {
@@ -38,4 +60,5 @@ app.UseUmbraco()
         u.UseWebsiteEndpoints();
     });
 
+// הרצת האפליקציה
 await app.RunAsync();
